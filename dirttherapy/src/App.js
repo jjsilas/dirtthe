e665a283eb95f1e81295969ed293a69a770842f5
Jherich Silas <jjsilas@gmail.com>
2020-08-29 23:23:48 -0500

    updated app.js with logo and temp text

--- dirttherapy/src/App.js
+++ dirttherapy/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './images/dirttherapy.tklogov1.png'
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dirt Therapy TK is a project covering some of the things I do to make it through the day.
         </p>
         <a
           className="App-link"
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I don't know exactly where it will go, but you're welcome to come along for the ride.
         </a>
       </header>
     </div>
