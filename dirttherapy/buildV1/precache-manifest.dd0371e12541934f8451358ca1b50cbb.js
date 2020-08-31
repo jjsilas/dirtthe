3b0f18b90f2f7dcd558d637036a1876e4549c897
Jherich Silas <jjsilas@gmail.com>
2020-08-30 23:27:24 -0500

    added favicon

---
+++ dirttherapy/buildV1/precache-manifest.dd0371e12541934f8451358ca1b50cbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856bbe71e79015b6474e400ac6bd12fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a4bfcf77a9edcf3249d",
+    "url": "/static/css/main.20b28327.chunk.css"
+  },
+  {
+    "revision": "a14f2131490db7b66063",
+    "url": "/static/js/2.9d8a233a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d8a233a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4bfcf77a9edcf3249d",
+    "url": "/static/js/main.ce7a7d69.chunk.js"
+  },
+  {
+    "revision": "e14a938d52feb95767b4",
+    "url": "/static/js/runtime-main.28dba077.js"
+  },
+  {
+    "revision": "e11c414bb2b77239b54df73927e87af6",
+    "url": "/static/media/dirttherapy.tklogov1.e11c414b.png"
+  }
+]);
--- dirttherapy/src/App.js
+++ dirttherapy/src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p id="openingText" >
