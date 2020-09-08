2379f8127e1dded6222172863976893016459292
Jherich Silas <jjsilas@gmail.com>
2020-09-08 00:59:01 -0500

    Updated Alt text for ally compliance

--- dirttherapy/src/App.js
+++ dirttherapy/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import logo from './images/dirttherapy.tklogov1.png'
 import './App.css';
+import ReactPlayer from "react-player"
 
 function App() {
   return (
@@ -9,10 +10,16 @@ function App() {
 
 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="Dirt Therapy Logo" />
         <p id="openingText" >
           Dirt Therapy TK is a project covering some of the things I do to make it through the day.
         </p>
+        <div>
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=8QT9bzEXEd0"
+          />
+        </div>
+
 
         <p id="comeAlong">
           I don't know exactly where it will go, but you're welcome to come along for the ride.
